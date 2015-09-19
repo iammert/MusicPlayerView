6ab9bc400f893aca71e58ff6537e0d5ac9c4b2e9
Pedro Paulo Amorim <pepa.amorim@gmail.com>
2015-09-19 00:02:54 -0300

    Minor changes

--- app/src/main/java/co/mobiwise/musicplayerprogressview/MainActivity.java
+++ app/src/main/java/co/mobiwise/musicplayerprogressview/MainActivity.java
@@ -8,29 +8,25 @@ import android.widget.ImageView;
 
 import co.mobiwise.library.MusicPlayerView;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends Activity{
+  MusicPlayerView mpv;
 
-    MusicPlayerView mpv;
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    mpv = (MusicPlayerView) findViewById(R.id.mpv);
+    mpv.setCoverURL("https://upload.wikimedia.org/wikipedia/en/b/b3/MichaelsNumberOnes.JPG");
 
-        mpv = (MusicPlayerView) findViewById(R.id.mpv);
-        mpv.setCoverURL("https://upload.wikimedia.org/wikipedia/en/b/b3/MichaelsNumberOnes.JPG");
-
-        mpv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (mpv.isRotating())
-                    mpv.stop();
-                else
-                    mpv.start();
-            }
-        });
-    }
-
+    mpv.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        if (mpv.isRotating()) {
+          mpv.stop();
+        } else {
+          mpv.start();
+        }
+      }
+    });
+  }
 }
